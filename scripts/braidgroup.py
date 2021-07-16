9690b15b070a34dd105da873e26e5ff518a7210d
Jack Haviland <havijw@umich.edu>
2021-07-16 00:55:24 -0400

    Fixed number of braid group generators

--- scripts/braidgroup.py
+++ scripts/braidgroup.py
@@ -3,11 +3,11 @@ from sympy.combinatorics.fp_groups import FpGroup
 from itertools import product
 
 def BraidGroup(n):
-    generators = str().join([f"s{str(i)}," for i in range(n)])
+    generators = str().join([f"s{str(i)}," for i in range(n-1)])
     F, *_ = free_group(generators)
     relations = \
         [ f's{i}*s{j}*s{i}**(-1)*s{j}**(-1)'
-        for i,j in product(range(n),range(n)) if abs(i-j)>1] + \
+        for i,j in product(range(n-1),range(n-1)) if abs(i-j)>1] + \
         [ f's{i}*s{i+1}*s{i}*s{i+1}**(-1)*s{i}**(-1)*s{i+1}**(-1)'
         for i in range(1,n-1) ]
     return FpGroup(F, relations)
